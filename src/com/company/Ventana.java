e28882970d1e1a7964c55e63107343fef84ac8bb
Americo <r.americo.quiroz@gmail.com>
2018-02-08 20:21:10 -0400

    Ejemplo de BorderLayout

---
+++ src/com/company/Ventana.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Ventana extends JFrame{
+    private JButton b1, b2, b3, b4, b5;
+
+    public Ventana(){
+        super("Ejemplo de BorderLayout");
+        setSize(500,600);
+
+        b1 = new JButton("Boton 1");
+        b2 = new JButton("Boton 2");
+        b3 = new JButton("Boton 3");
+        b4 = new JButton("Boton 4");
+        b5 = new JButton("Boton 5");
+
+        setLayout(new BorderLayout());
+
+        add(b1,BorderLayout.NORTH);
+        add(b2,BorderLayout.CENTER);
+        add(b3,BorderLayout.SOUTH);
+        add(b4,BorderLayout.EAST);
+        add(b5,BorderLayout.WEST);
+
+
+    }
+}
